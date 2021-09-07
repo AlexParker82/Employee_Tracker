aeda9cebf624f462b5a527e82644353a7ab7a6f8
Alex Parker <alex.n.parker82@gmail.com>
2021-09-07 14:45:39 -0400

    setting up dotenv

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 mysql.createConnection(
     {
-        host: '',
-        user: '',
-        password: '',
-        database: ''
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASS,
+        database: process.env.DB_NAME
     }
 ).then((connection) => {
     console.info('Connected to database.');
     const db = connection
 
-    db.query()
-})
+    db.query("SELECT * FROM emp_role")
+    .then((results) => console.log(results))
+    .catch((exc) => console.error(exc));
+
+}).catch((exc) => console.error(exc));
